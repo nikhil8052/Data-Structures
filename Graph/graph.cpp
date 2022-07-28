ff93d780f04ba86445480a65fee8b5cbdf77efd5
Nikhil <nikhilkumarnk1142@gmail.com>
2022-07-28 23:58:55 +0530

    Graph First Commit

---
+++ Graph/graph.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+// Graph class to build the Graph independently
+class Graph
+{
+
+public:
+	// Map to hold edges and vertexs
+	unordered_map<int, list<int>> nodes;
+
+	// Add new edge to the grpah
+	int add_edge(int vertex, int edge, bool bidirection)
+	{
+		nodes[vertex].push_back(edge);
+
+		// if bidirection is true then make bidirection communication
+		if (bidirection == true)
+		{
+			nodes[edge].push_back(vertex);
+		}
+	}
+
+	// Print the whole graph
+	void print()
+	{
+		for (auto vertex : nodes)
+		{
+			cout << vertex.first << "-->";
+			for (auto edges : vertex.second)
+			{
+				cout << edges << "-->";
+			}
+			cout << "\n";
+		}
+	}
+};
+
+int main()
+{
+
+	Graph graph;
+	graph.add_edge(1, 3, false);
+	graph.add_edge(1, 90, true);
+	graph.add_edge(2, 5, false);
+	graph.add_edge(5, 6, true);
+	graph.print();
+
+	// OUTPUT
+	/*
+
+	  6,1,5,90,2 are the vertexs and other are the edges connected to the vertexs;
+
+	  6-->5-->
+	  1-->3-->90-->
+	  5-->6-->
+	  90-->1-->
+	  2-->5-->
+
+	*/
+}
