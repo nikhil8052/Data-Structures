bb7fd6f7f2f01b7dde3ad834d9d18fcebfa1f14e
Nikhil <nikhilkumarnk1142@gmail.com>
2022-08-02 23:41:53 +0530

    Adding Questions

--- matrix/RotateMatrixClockWise.cpp
+++ matrix/RotateMatrixClockWise.cpp
@@ -1,28 +1,74 @@
+
+
+/*
+
+
+     1   2   3   4   5
+     6   7   8   9   10
+     11  12  13  14  15
+     16  17  18  19  20
+     21  22  23  24  25
+
+*/
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+// Rotate matrix
 void rotateMatrixClockWise(vector<vector<int>> &matrix) {
     // length of the matrix .
     int size = matrix.size();
+    int lastIndex = size - 1;
+    int curRing = 0;
 
-    //   Iterate over the first row
-    for (int i = size-1; i>=0; i--) {
-        int tem=matrix[0][i];
-        matrix[0][i]=matrix[0][i-1];
-        matrix[0][i]=tem;
+    while (curRing < (size / 2)) {
+        //   Iterate over the first row of ever ring and rotate it.
+        int pre = matrix[curRing + 1][curRing];
+        for (int i = curRing; i < size - curRing; i++) {
+            int cur = matrix[curRing][i];
+            matrix[curRing][i] = pre;
+            pre = cur;
+        }
 
-        // cout << matrix[i][size - 1] << " ";
-        // cout << matrix[size - 1][i] << " ";
-        // cout << matrix[i][0];
+        // Iterate over the last column of every ring and rotate it .
+        for (int i = curRing + 1; i < size - curRing; i++) {
+            int cur = matrix[i][lastIndex - curRing];
+            matrix[i][lastIndex - curRing] = pre;
+            pre = cur;
+        }
+
+        // Iterate over the last row of every ring and then rotate it .
+        for (int i = lastIndex - curRing - 1; i >= curRing; i--) {
+            int cur = matrix[lastIndex - curRing][i];
+            matrix[lastIndex - curRing][i] = pre;
+            pre = cur;
+        }
+
+        // Iterate over the first column of every ring and rotate it.
+        for (int i = lastIndex - curRing - 1; i >= curRing; i--) {
+            int cur = matrix[i][curRing];
+            matrix[i][curRing] = pre;
+            pre = cur;
+        }
+
+        curRing++;
     }
 
+    // Print the matrix.
+    cout << endl;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << matrix[i][j] << "  ";
+        }
+        cout << endl;
+    }
 }
 
+// Code starts from here
 int main() {
     int size;
-    cout << " Enter the matrix size. ";
+    cout << " Enter the Rows and Colums Size. ";
     cin >> size;
     vector<vector<int>> matrix(size, vector<int>(size));
 
@@ -33,11 +79,5 @@ int main() {
     }
 
     rotateMatrixClockWise(matrix);
-    cout << endl;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            cout << matrix[i][j]<<"  " ;
-        }
-        cout << endl;
-    }
+    cout << "Done";
 }
