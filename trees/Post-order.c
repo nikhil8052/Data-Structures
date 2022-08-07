bad72ea3e967dd1eecd6ece90c3fd58f51e61408
Nikhil <nikhilkumarnk1142@gmail.com>
2022-08-07 10:22:54 +0530

    Adding the trees Questions

---
+++ trees/Post-order.c
@@ -0,0 +1,86 @@
+
+/*
+
+    Written By : Nikhil 
+    Date :- 07 August 2022 
+    Brief :- Implementation of tree. 
+
+
+    It might seem you a bit complicated implementation.Because, I used refrences(Pointers) in this.
+    Easy one might be available on Google. 
+
+    Tip:- Please keep pen and paper and try to dry run the code. 
+
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h >
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *root = NULL;
+
+
+// Post-Order traversal 
+void postOrder(struct Node *root) {
+    if (root == NULL) {
+        return;
+    }
+
+    preOrder(root->left);
+    preOrder(root->right);
+    printf(" %d -> ", root->data);
+}
+
+
+struct Node *getNode(int data) {
+    struct Node *tem = (struct Node *)malloc(sizeof(struct Node));
+    tem->data = data;
+    tem->left = tem->right = NULL;
+    return tem;
+}
+
+int getInput() {
+    int ele;
+    printf(" Enter the Value ");
+    scanf(" %d", &ele);
+    return ele;
+}
+
+// Construct the Tree 
+void constructTree(struct Node **root) {
+    if (*root == NULL) {
+        *root = getNode(getInput());
+    }
+
+    char ch;
+    printf(" Now , Root value is %d \n ", (*root)->data);
+    printf(" Do you want to enter left child of %d \n Y/N ", (*root)->data);
+    scanf(" %c", &ch);
+
+    if (ch == 'y' || ch == 'Y') {
+        (*root)->left = getNode(getInput());
+        constructTree(&(*root)->left);
+    }
+
+    printf(" Now , Root value is %d \n ", (*root)->data);
+    printf(" Do you want to enter Right  child of %d \n Y/N ", (*root)->data);
+    scanf(" %c", &ch);
+
+    if (ch == 'y' || ch == 'Y') {
+        (*root)->right = getNode(getInput());
+        constructTree(&(*root)->right);
+    }
+}
+
+
+
+int main() {
+    constructTree(&root);
+    postOrder(root);
+}
---
+++ trees/README.md
@@ -0,0 +1,9 @@
+# Tress 
+
+1 . Implementation of Tree .
+
+2. In-order Traversal 
+
+3. Pre-order Traversal 
+
+4. Post-order Traversal 
